bfab429f2c1225696101d1667c7e8561db40810d
conquerer-wo <remiss_suit.4d@icloud.com>
2025-06-13 12:34:43 -0400

    starting to make the bulk of the app

--- react-testing/src/search.jsx
+++ react-testing/src/search.jsx
@@ -1,14 +1,45 @@
-import React from "react"
-import { useState } from 'react';
-import "./search.css"
-function Search() { const [input, setInput] = useState(""); async function Api() { const apiKey="32bcfa90"  ;const url =`http://www.omdbapi.com/?apikey=${apiKey}&s=${input}`; 
-try {const response = await fetch(url)
-const res =  await response.json()
-if(!response.ok) {window.alert("not Found")}
-console.log(res.Search) }
-catch(error) {window.alert("not working");  console.error(error);}
+import React, { useState } from "react";
+import "./search.css";
 
+function Search() {
+  const [input, setInput] = useState("");
 
-}; return (<div className="cont"><input onChange = {(e)=> {setInput(e.target.value)}}id = "search" placeholder="search a movie"></input><button id="searchBtn" onClick = {()=> {Api()}}>search</button></div>)}
+  async function Api() {
+    const apiKey = "32bcfa90";
+    const url = `http://www.omdbapi.com/?apikey=${apiKey}&s=${input}`;
 
-export default Search 
+    try {
+      const response = await fetch(url);
+      const res = await response.json();
+
+      if (!response.ok) {
+        window.alert("Not Found");
+      }
+
+      console.log(res.Search);
+      const movTitle = res.Search.slice(0,10).Title;
+      const movImg = res.Search.Poster;
+      const movType = res.Search.Type;
+      const movYear = res.Search.Year;
+      console.log(movTitle)
+    } catch (error) {
+      window.alert("Not working");
+      console.error(error);
+    }
+  }
+        
+  return (
+    <div className="cont">
+      <input
+        id="search"
+        placeholder="search a movie"
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button id="searchBtn" onClick={() => Api()}>
+        search
+      </button>
+    </div>
+  );
+}
+
+export default Search;
