b59a559c075b5da4f83fc0fd12913f9c190b2ae2
conquerer-wo <remiss_suit.4d@icloud.com>
2025-06-13 18:20:01 -0400

    Update vite.config.js
    made it work on GitHub Pages

--- react-testing/vite.config.js
+++ react-testing/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/react-testing-ground/',  // 👈 ADD THIS LINE — MUST match your repo name
   server: {
     open: {
       app: {
