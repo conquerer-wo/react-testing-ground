cc266b8012ddbff5e74a1850cc1c0c47d908a6da
conquerer-wo <remiss_suit.4d@icloud.com>
2025-06-13 09:55:32 -0400

    set up foundation for search feature

--- react-testing/src/search.jsx
+++ react-testing/src/search.jsx
@@ -1,5 +1,6 @@
 import React from "react"
+import { useState } from 'react';
 import "./search.css"
-function Search() {return (<div className="cont"><input id = "search" placeholder="search a movie"></input><button id="searchBtn">search</button></div>)}
+function Search() { const [input, setInput] = useState("");return (<div className="cont"><input onChange = {(e)=> {setInput(e.target.value)}}id = "search" placeholder="search a movie"></input><button id="searchBtn">search</button></div>)}
 
 export default Search 
